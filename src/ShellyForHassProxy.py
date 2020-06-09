e5a622ba12db3594d8e57c60dbcf20c31063011d
Robin Östlund <me@robinostlund.name>
2020-06-09 11:08:17 +0200

    initial

--- src/ShellyForHassProxy.py
+++ src/ShellyForHassProxy.py
@@ -2,20 +2,6 @@ import argparse
 import socket
 import struct
 
-#Proxy script that receive CoAP message and forward them to Shelly HASS plugin.
-#Use this if you have Shelly devices on other sub-net or VLAN
-
-#Require ShellyForHASS 0.0.15 or later
-
-#Change to the ip-address of your HASS server
-HASS_IP = "127.0.0.1"
-HASS_PORT = 5684
-
-UDP_IP = "224.0.1.187"
-UDP_PORT = 5683
-
-
-
 def shelly_proxy(hass_ip, hass_port, udp_ip, udp_port):
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
   sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -24,13 +10,13 @@ def shelly_proxy(hass_ip, hass_port, udp_ip, udp_port):
   sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
   while True:
     try:
-      #Receive CoAP message
+      # Receive CoAP message
       data, addr = sock.recvfrom(10240)
-      #Tag and add device ip-address to message
+      # Tag and add device ip-address to message
       newdata = bytearray(b'prxy')
       newdata.extend(socket.inet_aton(addr[0]))
       newdata.extend(data)
-      #Send to Shelly plugin
+      # Send to Shelly plugin
       sock.sendto(newdata, (hass_ip, hass_port))
     except Exception as e:
       print ('exception ' + str(e))
