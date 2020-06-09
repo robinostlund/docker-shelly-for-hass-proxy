144823f1307ef5921fe4630118b661ed01bc6e59
Robin Östlund <me@robinostlund.name>
2020-06-09 12:55:48 +0200

    initial

--- src/ShellyForHassProxy.py
+++ src/ShellyForHassProxy.py
@@ -14,8 +14,8 @@ class ShellyProxy:
     # bind socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(('', udp_port))
-    mreq = struct.pack("4sl", socket.inet_aton(udp_ip), socket.INADDR_ANY)
+    sock.bind(('', self.udp_port))
+    mreq = struct.pack("4sl", socket.inet_aton(self.udp_ip), socket.INADDR_ANY)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
     # start loop
@@ -32,7 +32,7 @@ class ShellyProxy:
         newdata.extend(socket.inet_aton(addr[0]))
         newdata.extend(data)
         # Send to Shelly plugin
-        sock.sendto(newdata, (hass_ip, hass_port))
+        sock.sendto(newdata, (self.hass_ip, self.hass_port))
       except Exception as e:
         print ('exception ' + str(e))
 
