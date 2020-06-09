4a40cad72a26918cf7f2d1756ffe029be014491f
Robin Östlund <me@robinostlund.name>
2020-06-09 11:44:05 +0200

    development

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,14 @@ if [ "${DEBUG}" == "true" ]; then
   set -x
 fi
 
+# get environment variables and set default ones
 HOMEASSISTANT_IP=${HASS_IP:=127.0.0.1}
 HOMEASSISTANT_PORT=${HASS_PORT:=5683}
 PROXY_UDP_IP=${PROXY_IP:=224.0.1.187}
 PROXY_UDP_PORT=${PROXY_PORT:=5683}
 
-# Start proxy
- python3 /opt/proxy.py
+# set environment variables
+export HOMEASSISTANT_IP HOMEASSISTANT_PORT PROXY_UDP_IP PROXY_UDP_PORT
+
+# start proxy
+python3 /opt/proxy.py
