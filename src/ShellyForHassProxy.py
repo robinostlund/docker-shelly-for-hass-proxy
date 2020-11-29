97bb0089473f6137ffbf887af1512fc914cb8b58
Robin Östlund <me@robinostlund.name>
2020-11-29 15:05:35 +0100

    smaller changes

--- src/ShellyForHassProxy.py
+++ src/ShellyForHassProxy.py
@@ -1,6 +1,7 @@
 import os
 import socket
 import struct
+import logging
 
 from datetime import datetime 
 
@@ -12,14 +13,12 @@ class ShellyProxy:
     self.coap_port = coap_port
     self.debug = debug
 
-  def pprinter(self, msg):
-    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    print(f'[{now}] {msg}')
+    # if self.debug and self.debug == 'yes':
 
   def run(self):
     # bind socket
-    self.pprinter(f'Starting ShellyForHASS Proxy..')
-    self.pprinter(f'Listening on {self.coap_port} for messages from {self.coap_ip}')
+    logging.info(f'Starting ShellyForHASS Proxy..')
+    logging.info(f'Listening on {self.coap_port} for messages from {self.coap_ip}')
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind(('', self.coap_port))
@@ -27,14 +26,13 @@ class ShellyProxy:
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
     # start loop
-    self.pprinter(f'Started ShellyForHASS Proxy and forwarding messages to {self.hass_ip}:{self.hass_port}')
+    logging.info(f'Started ShellyForHASS Proxy and forwarding messages to {self.hass_ip}:{self.hass_port}')
     while True:
       try:
         # Receive CoAP message
         data, addr = sock.recvfrom(10240)
         # Debug
-        if self.debug and self.debug == 'yes':
-          self.pprinter(f'Got CoAP message from: {addr[0]}:{addr[1]}')
+        logging.debug(f'Got CoAP message from: {addr[0]}:{addr[1]}')
         # Tag and add device ip-address to message
         newdata = bytearray(b'prxy')
         newdata.extend(socket.inet_aton(addr[0]))
@@ -42,7 +40,7 @@ class ShellyProxy:
         # Send to Shelly plugin
         sock.sendto(newdata, (self.hass_ip, self.hass_port))
       except Exception as e:
-        print('exception ' + str(e))
+        logginf.error('exception ' + str(e))
 
 
 def main():
