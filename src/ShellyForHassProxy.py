b09df3a3b6e05d6bedb8100251ce4d50200e8b81
Robin Östlund <me@robinostlund.name>
2020-11-29 15:41:36 +0100

    smaller changes

--- src/ShellyForHassProxy.py
+++ src/ShellyForHassProxy.py
@@ -14,7 +14,10 @@ class ShellyProxy:
     self.debug = debug
 
     # configure logging
-    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+    logging.basicConfig(
+      format='%(asctime)s.%(msecs)03d %(levelname)s: %(message)s',
+      datefmt='%Y-%m-%d %H:%M:%S',
+      level=logging.INFO)
     if self.debug and self.debug == 'yes':
       logging.getLogger().setLevel(logging.DEBUG)
 
